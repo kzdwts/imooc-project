3f858394e00a93847c1aa5737514a2928eb9f25b
kangyong <kangyong_wts@sina.com>
2020-04-15 21:38:47 +0800

    commit

---
+++ imooc-socket/src/main/java/com/imooc/project/socket/ImoocSocketApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.project.socket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocSocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocSocketApplication.class, args);
+    }
+
+}
---
+++ imooc-socket/src/test/java/com/imooc/project/socket/ImoocSocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.project.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocSocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
