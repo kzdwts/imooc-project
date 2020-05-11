9d02cbdc5ba01d16d85a3d6eb3ee8828187335aa
kangyong <kangyong_wts@sina.com>
2020-05-11 13:05:02 +0800

    新增mybatis-plus编程

---
+++ imooc-mp/src/main/java/com/imooc/mp/ImoocMpApplication.java
@@ -0,0 +1,13 @@
+package com.imooc.mp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImoocMpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImoocMpApplication.class, args);
+    }
+
+}
---
+++ imooc-mp/src/test/java/com/imooc/mp/ImoocMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.imooc.mp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoocMpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
